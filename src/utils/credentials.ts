9d9e0f049afb17a79db7c4c9d539f58330bca1e8
danifosy <mail@daniha.de>
2021-08-12 16:17:50 +0200

    write encryptCredential function and import into credentials.ts

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,6 +1,6 @@
 import { readFile, writeFile } from 'fs/promises';
-
 import { Credential, DB } from '../types';
+import { encryptCredential } from './crypto';
 
 export async function readCredentials(): Promise<Credential[]> {
   const response = await readFile('src/db.json', 'utf-8');
@@ -14,23 +14,23 @@ export async function getCredential(service: string): Promise<Credential> {
   const credential = credentials.find(
     (credential) => credential.service === service
   );
-
   if (!credential) {
     throw new Error(`No credential found for service: ${service}`);
   }
-
   return credential;
 }
-
 export async function addCredential(credential: Credential): Promise<void> {
   // reads all creds from db
   const credentials = await readCredentials();
+
   // spreads all old creds and adds new creds and creates array of them
-  const newCredentials = [...credentials, credential];
+  const newCredentials = [...credentials, encryptCredential(credential)];
+
   // the key we fill with information. in this key we give all our info
   const newDB: DB = {
     credentials: newCredentials,
   };
+
   // writes new credential as a string bc it expects a string
   await writeFile('src/db.json', JSON.stringify(newDB, null, 2));
 }
@@ -62,7 +62,7 @@ export async function updateCredential(
   //overwrite DB
 
   const newDB: DB = {
-    credentials: [...filteredCredentials, credential],
+    credentials: [...filteredCredentials, encryptCredential(credential)],
   };
   await writeFile('src/db.json', JSON.stringify(newDB, null, 2));
 }

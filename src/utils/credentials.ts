534137df6d62f84411a48e6480c70e1fe7786b7b
danifosy <mail@daniha.de>
2021-08-12 21:11:11 +0200

    Add comments to functions

--- src/utils/credentials.ts
+++ src/utils/credentials.ts
@@ -1,24 +1,34 @@
 import { readFile, writeFile } from 'fs/promises';
-import { Credential, DB } from '../types';
-import { encryptCredential } from './crypto';
+import type { Credential, DB } from '../types';
+import { encryptCredential, decryptCredential } from './crypto';
 
+// reads db.json and turns them into usable data
 export async function readCredentials(): Promise<Credential[]> {
+  // reads db file and saves the content in the var "response" as a string
   const response = await readFile('src/db.json', 'utf-8');
+  // puts string into object so we can continue using it
   const db: DB = JSON.parse(response);
+  // gives back array inside of the object
   const credentials = db.credentials;
   return credentials;
 }
 
-export async function getCredential(service: string): Promise<Credential> {
+export async function getCredential(serviceName: string): Promise<Credential> {
+  // calls readCredentials() function and saves the array in var
   const credentials = await readCredentials();
+  // takes the cred data and finds the fitting service according to the service we give into the function
   const credential = credentials.find(
-    (credential) => credential.service === service
+    (credential) => credential.service === serviceName
   );
+  // if cred is not found, throw this error
   if (!credential) {
-    throw new Error(`No credential found for service: ${service}`);
+    throw new Error(`No credential found for service: ${serviceName}`);
   }
-  return credential;
+  const decryptedCredential = decryptCredential(credential);
+
+  return decryptedCredential;
 }
+
 export async function addCredential(credential: Credential): Promise<void> {
   // reads all creds from db
   const credentials = await readCredentials();
@@ -35,12 +45,12 @@ export async function addCredential(credential: Credential): Promise<void> {
   await writeFile('src/db.json', JSON.stringify(newDB, null, 2));
 }
 
-export async function deleteCredential(service: string): Promise<void> {
+export async function deleteCredential(serviceName: string): Promise<void> {
   //reads exisiting creds
   const credentials = await readCredentials();
   //modifies credentials so that it's everything except the filtered cred
   const filteredCredentials = credentials.filter(
-    (credential) => credential.service !== service
+    (credential) => credential.service !== serviceName
   );
   //new creds without the filtered cred
   const newDB: DB = {
@@ -50,14 +60,14 @@ export async function deleteCredential(service: string): Promise<void> {
 }
 
 export async function updateCredential(
-  service: string,
+  serviceName: string,
   credential: Credential
 ): Promise<void> {
   //get all credentials
   const credentials = await readCredentials();
   //modify one
   const filteredCredentials = credentials.filter(
-    (credential) => credential.service !== service
+    (credential) => credential.service !== serviceName
   );
   //overwrite DB
 

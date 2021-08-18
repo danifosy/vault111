40b1ae054f7b7fff63a87de1ba4ae051a4ed66d5
danifosy <mail@daniha.de>
2021-08-18 16:59:16 +0200

    Fix syntax

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,45 @@
-import './style.css';
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import styles from './App.module.css';
 
-const app = document.querySelector<HTMLDivElement>('#app');
+function App(): JSX.Element {
+  const [count, setCount] = useState<number>(0);
 
-if (app !== null) {
-  app.innerHTML = `
-  <h1>Hello Vite!</h1>
-  <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a>
-`;
+  return (
+    <div className={styles.App}>
+      <header className={styles['App-header']}>
+        <img src={logo} className={styles['App-logo']} alt="logo" />
+        <p>Hello Vite + React!</p>
+        <p>
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is: {count}
+          </button>
+        </p>
+        <p>
+          Edit <code>App.tsx</code> and save to test HMR updates.
+        </p>
+        <p>
+          <a
+            className={styles['App-link']}
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+          {' | '}
+          <a
+            className={styles['App-link']}
+            href="https://vitejs.dev/guide/features.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Vite Docs
+          </a>
+        </p>
+      </header>
+    </div>
+  );
 }
+
+export default App;

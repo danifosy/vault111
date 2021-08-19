3ed4a9870c2856c9f1ba8b505c4b078aa9fc1cd2
danifosy <mail@daniha.de>
2021-08-19 11:16:02 +0200

    Build Route and switch into App.tsx

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Dashboard from './pages/dashboard/Dashboard';
 
 function App(): JSX.Element {
-  return <Dashboard />;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <Dashboard />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;

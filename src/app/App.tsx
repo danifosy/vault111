81274ecad3840dfa2696adb6cc53a6b8fe60b029
danifosy <mail@daniha.de>
2021-08-19 11:57:46 +0200

    Add Password Route

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Dashboard from './pages/dashboard/Dashboard';
+import Password from './pages/Password/Password';
 
 function App(): JSX.Element {
   return (
     <BrowserRouter>
       <Switch>
+        <Route path="/password/:service">
+          <Password />
+        </Route>
         <Route exact path="/">
           <Dashboard />
         </Route>

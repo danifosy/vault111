4df0642a6c2167cef381e557a5f36f08cb18e42c
danifosy <mail@daniha.de>
2021-08-20 19:41:28 +0200

    Add header

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import styles from './App.module.css';
 
 import Dashboard from './pages/dashboard/Dashboard';
 import Password from './pages/Password/Password';
@@ -7,22 +8,25 @@ import AddCredential from './pages/AddCredentials/AddCredentials';
 
 function App(): JSX.Element {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/password/:service">
-          <Password />
-        </Route>
-        <Route path="/addCredential">
-          <AddCredential />
-        </Route>
-        <Route path="/searchCredential">
-          <AddCredential />
-        </Route>
-        <Route exact path="/">
-          <Dashboard />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <>
+      <h1 className={styles.header}>Password manager</h1>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/password/:service">
+            <Password />
+          </Route>
+          <Route path="/addCredential">
+            <AddCredential />
+          </Route>
+          <Route path="/searchCredential">
+            <AddCredential />
+          </Route>
+          <Route exact path="/">
+            <Dashboard />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </>
   );
 }
 

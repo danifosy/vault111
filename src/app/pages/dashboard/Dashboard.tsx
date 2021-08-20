07e1898c0954976dc7a8e6dd52d99698d019f14b
danifosy <mail@daniha.de>
2021-08-20 19:40:51 +0200

    Add conditional to return

--- src/app/pages/dashboard/Dashboard.tsx
+++ src/app/pages/dashboard/Dashboard.tsx
@@ -28,33 +28,39 @@ export default function Dashboard(): JSX.Element {
     // if you put variables into the [], the use effect function will always be called when the value of the vars changes
   }, [masterpassword]);
 
-  return (
-    <main>
-      <h1 className={styles.header}>Password manager</h1>
-      <h2 className={styles.subHeader}>
-        Your personal password manager powered by caffeine and sweat!
-      </h2>
+  if (credentials.length !== 0) {
+    return (
+      <main>
+        {credentials.length !== 0 &&
+          credentials.map((credential) => (
+            <CredentialCard credentialData={credential} />
+          ))}
+        <Link to="/addCredential">
+          <img src="assets/addButton.svg" className={styles.addButton} />
+        </Link>
+        <Link to="/searchCredential">
+          <img src="assets/searchButton.svg" className={styles.searchButton} />
+        </Link>
+      </main>
+    );
+  } else {
+    return (
+      <main>
+        <h2 className={styles.subHeader}>
+          Your personal password manager powered by caffeine and sweat!
+        </h2>
 
-      <div className={styles.container}>
-        <input
-          type="password"
-          placeholder="Masterpassword"
-          className={styles.containerItems}
-          value={masterpassword}
-          onChange={(event) => setMasterpassword(event.target.value)}
-        />
-        <button className={styles.containerButton}>Login</button>
-      </div>
-      {credentials.length !== 0 &&
-        credentials.map((credential) => (
-          <CredentialCard credentialData={credential} />
-        ))}
-      <Link to="/addCredential">
-        <img src="assets/addButton.svg" className={styles.addButton} />
-      </Link>
-      <Link to="/searchCredential">
-        <img src="assets/searchButton.svg" className={styles.searchButton} />
-      </Link>
-    </main>
-  );
+        <div className={styles.container}>
+          <input
+            type="password"
+            placeholder="Masterpassword"
+            className={styles.containerItems}
+            value={masterpassword}
+            onChange={(event) => setMasterpassword(event.target.value)}
+          />
+          <button className={styles.containerButton}>Login</button>
+        </div>
+      </main>
+    );
+  }
 }

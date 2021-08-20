736d3b1a192c75581e701a2024b52bb39b656fcd
danifosy <mail@daniha.de>
2021-08-20 19:41:18 +0200

    Delete header

--- src/app/pages/AddCredentials/AddCredentials.tsx
+++ src/app/pages/AddCredentials/AddCredentials.tsx
@@ -1,42 +1,80 @@
 import React, { useState } from 'react';
+import { Credential } from '../../../types';
+
+const emptyCredential = {
+  service: '',
+  username: '',
+  password: '',
+};
 
 export default function AddCredential(): JSX.Element {
-    
-        const [masterpassword, setMasterpassword] = useState("");
+  const [credential, setCredential] = useState<Credential>(emptyCredential);
+  const [masterpassword, setMasterpassword] = useState('');
 
-async function addCredential() {
-
-    const response = await fetch("/api/credentials"), {
+  const submit = () => {
+    async function fetchCredentials() {
+      const response = await fetch('/api/credentials', {
+        method: 'POST',
         headers: {
-            Authorization: masterpassword,
-        }
+          Authorization: masterpassword,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(credential),
+      });
+      await response.json();
+      setCredential(emptyCredential);
     }
-    
-}
-    
-}
-
+    fetchCredentials();
+  };
 
   return (
     <>
-      <div>
+      <form
+        onSubmit={(event) => {
+          event?.preventDefault();
+          submit();
+        }}
+      >
         <p>Add a service</p>
         <label>
-          <input type="text" />
+          <input
+            type="text"
+            value={credential.service}
+            onChange={(event) =>
+              setCredential({ ...credential, service: event.target.value })
+            }
+          />
         </label>
         <p>Add a username</p>
         <label>
-          <input type="text" />
+          <input
+            type="text"
+            value={credential.username}
+            onChange={(event) =>
+              setCredential({ ...credential, username: event.target.value })
+            }
+          />
         </label>
         <p>Add a password</p>
         <label>
-          <input type="text" />
+          <input
+            type="text"
+            value={credential.password}
+            onChange={(event) =>
+              setCredential({ ...credential, password: event.target.value })
+            }
+          />
         </label>
         <p>Please enter master password</p>
         <label>
-          <input type="password" />
+          <input
+            type="password"
+            value={masterpassword}
+            onChange={(event) => setMasterpassword(event.target.value)}
+          />
         </label>
-      </div>
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
